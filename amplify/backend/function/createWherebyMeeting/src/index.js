054f489bcb7c5de6ba03366277d53986ac01b83e
Michi <michael.peschel@mip-sw.de>
2021-09-24 11:30:46 +0200

    cleaned up Whereby API

--- amplify/backend/function/createWherebyMeeting/src/index.js
+++ amplify/backend/function/createWherebyMeeting/src/index.js
@@ -1,29 +1,5 @@
-const aws = require('aws-sdk');
-async function getParameters() {
-    console.log(process.env);
-    const { Parameters } = await (new aws.SSM())
-        .getParameters({
-            Names: ["WHEREBY_API_KEY"].map(secretName => process.env[secretName]),
-            WithDecryption: true,
-        }).promise();
-
-    console.log(Parameters);
-    console.log(process.env);
-
-    return Parameters;
-}
-
-
-const parameters = getParameters();
-
 const awsServerlessExpress = require('aws-serverless-express');
 const app = require('./app');
-app.set('WHEREBY_API_KEY', parameters);
-
-
-
-
-
 const server = awsServerlessExpress.createServer(app);
 
 exports.handler = (event, context) => {
